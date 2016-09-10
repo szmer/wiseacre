78f0ae8ebda5c878e56f8c34ae531ee517f3387e
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2016-09-10 23:39:06 +0200

    ngrams as (Markov) chains of nodes with assigned probabilities

--- chatter.js
+++ chatter.js
@@ -1,39 +1,97 @@
 function makeRoom (match) { return ' '+match }
 function movePunctuation (str) { return str.replace(/[,:;()]/g, makeRoom) }
 
-function repeatStr(str, times) {
-    var ret = str
-    for(var i = 1; i < times; i++) ret += str
+// repeat returns an array of the elem repeated times times.
+function repeat(elem, times) {
+    var ret = [ elem ]
+    for(var i = 1; i < times; i++) ret.push( elem )
     return ret
 }
 
-function updFreqEntry (obj, key) {
-    if (key in obj)
-        obj[key] ++
-    else
-        obj[key] = 1
+// updNGramEntry increases the probability of tokens[0] by addProb and passes the rest of the tokens
+// array (if tokens.slice(1).length > 0) to the recursive calls of updNGramEntry, moving in the
+// requested direction. Thus this function increases the probability of the main entry and frequen-
+// cies of its descendants (which should be normalized later to obtain the actual probabilities).
+// The tokens.length of the initial call should be equal n in your n-gram table.
+function updNGramEntry (entry, tokens, direction, addProb) {
+    entry.prob += addProb
+
+    // Regardless of the requested addProb, pass down 1.0, indicating that downstream probabilities
+    // have to be normalized later (for them we can't figure out the number of samples just yet).
+    if (tokens.length > 1) {
+        if (! entry[direction].hasOwnProperty(tokens[1]))
+            entry[direction][tokens[1]] = new NGramNode
+        updNGramEntry(entry[direction][tokens[1]], tokens.slice(1), direction, 1.0)
+    }
 }
 
-function nGramFreqs (textStr, n) {
-    var freqs = Object()
+// normalizeNGramEntry normalizes probabilities of the entry's descendants in the requested
+// direction, and invokes normalizeNGramEntry function for each of them.
+function normalizeNGramEntry (entry, direction) {
+    var nextTotal = 0.0
+    for (var k in entry[direction])
+        nextTotal += entry[direction][k].prob
+    for (var k in entry[direction]) {
+        entry[direction][k].prob /= nextTotal
+        normalizeNGramEntry(entry[direction][k], direction)
+    }
+}
+
+function NGramNode(form) {
+    // Our probability of being in this state, given the previous state.
+    this.prob = 0.0
+
+    // From each of the core nodes in a n-gram model we have paths of n-1 next/previous states. In
+    // the end states these variables should remain empty objects.
+    this.forw = {} // possible next states (i.e. words)
+    this.backw = {} // possible previous states (i.e. words)
+}
+
+// nGrams return an object, where the labels are tokens which can be found in the textStr. To each
+// token the function assigns a chain of NGramNode objects of length n (including the main node),
+// leading forward and backward. The ?, !. . characters are treated as sentence delimeters and remo-
+// ved. At the sentence boundaries the function inserts dummy . entries to make sure that n-length
+// rule is preserved.
+function nGrams (textStr, n) {
+    var ngrams = Object()
 
     // replace !s and ?s with dots, then split by dots
     var sents = textStr.replace(/[!?]/g, '.').split('.')
+    var sent_tokens = [] // list of tokens for each sentence
 
-    for (s in sents) {
-        var sent = sents[s]
-        var tokens = movePunctuation(sent).split(' ')
+    var wordTotal = 0
+    for (var s in sents) {
+        sent_tokens[s] = movePunctuation(sents[s]).split(' ')
 
-        // for each of the actual n-grams in the sentence, increment its entry:
-        for (var i = 0; i < tokens.length-(n-1); i++)
-            updFreqEntry(freqs, tokens.slice(i, i+n).join(' '))
+        wordTotal += sent_tokens[s].length
 
-        // for each of the remaining words, make for them fake n-grams with dots as dummy words
-        // (note that we already removed the actual in-text dots)
-        for (var i = tokens.length-(n-1); i < tokens.length; i++)
-            updFreqEntry(freqs, tokens.slice(i).join(' ')+repeatStr(' .', tokens.length-i))
+        // Ensure that each wprd has its NGramNode entry.
+        for (var w in sent_tokens[s])
+            if (! ngrams.hasOwnProperty(sent_tokens[s][w]))
+                ngrams[sent_tokens[s][w]] = new NGramNode()
     }
 
-    return freqs
-}
+    // This is a probability unit that each word will receive for its one occurrence.
+    var probUnit =  1.0 / wordTotal
 
+    // Build the ngram chains from words of each sentence.
+    for (var s in sent_tokens) {
+        // Add dots at the sentence boundaries, so they will have some representation in our ngrams.
+        // (note that we already removed the actual in-text dots)
+        var tokens = repeat('.', n-1) .concat(sent_tokens[s], repeat('.', n-1))
+
+        for (var i = n-1; i < tokens.length-(n-1); i++)
+            updNGramEntry(ngrams[tokens[i]], tokens.slice(i, i+n), 'forw', probUnit)
+        for (var i = tokens.length-n; i >= n-1; i--)
+            // Here we don't want to increase the probability, since we've already made it above.
+            updNGramEntry(ngrams[tokens[i]], tokens.slice(i-n+1, i+1).reverse(), 'backw', 0.0)
+    }
+
+    // Normalize the probabilities of the embedded paths.
+    for (var k in ngrams) {
+        normalizeNGramEntry(ngrams[k], 'forw')
+        normalizeNGramEntry(ngrams[k], 'backw')
+    }
+
+    return ngrams
+}
--- test.js
+++ test.js
@@ -18,32 +18,43 @@ function testFunc (func, tests) {
 function tests () {
     console.log('Running tests for Chatter.')
 
-    testFunc(repeatStr,
-            ["repeatStr('bcd', 3) == 'bcdbcdbcd'"])
-    testFunc(makeRoom,
-             ["makeRoom('tty') == ' tty'",
-              "makeRoom(' tele fax') == '  tele fax'"])
-    testFunc(movePunctuation,
-             ["movePunctuation(', abba, cdda!') == ' , abba , cdda !'",
-              "movePunctuation(',,, zyxyxy: yeah') == ' , , , zyxyxy : yeah'"])
-    testFunc(updFreqEntry,
-             ["var o = Object(); updFreqEntry(o, 'aa'); o['aa'] == 1",
-              "var o = Object(); updFreqEntry(o, 'aa'); updFreqEntry(o, 'aa'); o['aa'] == 2"])
-    testFunc(nGramFreqs,
-             [
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda', 3); (o['zyxyxy telefax angh'] == o['telefax angh cdda'] == 1)",
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda zyxyxy telefax angh', 3); (o['zyxyxy telefax angh'] == 2 && o['angh cdda zyxyxy'] == o['telefax angh cdda'] == 1)",
-                 // punctuation:
-                 "var o = nGramFreqs('zyxyxy telefax angh; cdda: zyxyxy telefax angh;', 3); (o['telefax angh ;'] == 2 && o['; cdda :'] == o['cdda : zyxyxy'] == 1)",
-                 // dummy n-grams:
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda zyxyxy telefax angh', 3); (o['telefax angh .'] == o['angh . .'] == 1)",
-                 // multiple sentences:
-                 "var o = nGramFreqs('zyxyxy telefax. angh cdda! zyxyxy telefax angh.', 3); (o['zyxyxy telefax .'] == undefined && o['angh cdda .'] == o['telefax angh .'] == 1)",
-                 // for some n's /= 3
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda', 1); (o['zyxyxy'] == o['angh'] == 1)",
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda', 2); (o['zyxyxy telefax'] == o['angh cdda'] == 1)",
-                 "var o = nGramFreqs('zyxyxy telefax angh cdda', 5); (o['zyxyxy telefax angh cdda .'] == o['cdda . . . .'] == 1)",
-             ])
+    testFunc(repeat, [
+        "var l = repeat('bcd', 3); l.join('') == 'bcdbcdbcd'"
+    ])
+    testFunc(makeRoom, [
+        "makeRoom('tty') == ' tty'",
+        "makeRoom(' tele fax') == '  tele fax'"
+    ])
+    testFunc(movePunctuation, [
+        "movePunctuation(', abba, cdda;') == ' , abba , cdda ;'",
+        "movePunctuation(',,, zyxyxy: yeah') == ' , , , zyxyxy : yeah'"
+    ])
+    testFunc(updNGramEntry, [
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); 'bb' in o.forw",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); ! ('aa' in o.forw)",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); o.prob == 0.1",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); o.forw['bb'].prob == 1.0",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); o.backw['bb'].backw['cc'].prob == 1.0",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); updNGramEntry(o, ['aa', 'bb'], 'forw', 0.1); o.prob == 0.2",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); o.backw['bb'].backw['cc'].prob == 2.0",
+    ])
+    testFunc(normalizeNGramEntry, [
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); updNGramEntry(o, ['aa', 'bb', 'dd'], 'backw', 0.1); normalizeNGramEntry(o, 'backw'); o.backw['bb'].backw['cc'].prob == 0.5 && o.backw['bb'].backw['dd'].prob == 0.5",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); updNGramEntry(o, ['aa', 'bb', 'dd'], 'backw', 0.1); normalizeNGramEntry(o, 'backw'); o.backw['bb'].prob == 1.0",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); updNGramEntry(o, ['aa', 'cc', 'dd'], 'backw', 0.1); normalizeNGramEntry(o, 'backw'); o.backw['bb'].prob == 0.5 && o.backw['bb'].backw['cc'].prob == 1.0",
+        "var o = new NGramNode('aa'); updNGramEntry(o, ['aa', 'bb', 'cc'], 'backw', 0.1); updNGramEntry(o, ['aa', 'bb', 'dd'], 'backw', 0.1); normalizeNGramEntry(o, 'backw'); o.prob == 0.2",
+    ])
+    testFunc(nGrams, [
+        "var o = new nGrams('zyxyxy telefax angh cdda', 3); (o['zyxyxy'].prob == 0.25)",
+        "var o = new nGrams('zyxyxy telefax angh cdda', 3); (o['zyxyxy'].prob == o['cdda'].prob)",
+        "var o = new nGrams('zyxyxy telefax angh cdda zyxyxy telefax angh', 3); (o['zyxyxy'].prob == 2/7 && o['cdda'].prob == 1/7)",
+        "'farther nodes'; var o = new nGrams('zyxyxy telefax angh cdda zyxyxy telefax tty', 3); (o['zyxyxy'].forw['telefax'].forw['angh'].prob == 0.5 && o['zyxyxy'].forw['telefax'].forw['tty'].prob == 0.5)",
+        "'backward nodes'; var o = new nGrams('zyxyxy telefax angh cdda zyxyxy telefax tty', 3); (o['angh'].backw['telefax'].backw['zyxyxy'].prob == 1.0)",
+        "'punctuation'; var o = new nGrams('zyxyxy telefax angh; cdda: zyxyxy telefax angh;', 3); (o['telefax'].forw['angh'].forw[';'].prob == 1.0 && o[';'].forw['cdda'].forw[':'].prob == 1.0)",
+        "'dot/boundary nodes'; var o = new nGrams('zyxyxy telefax angh cdda zyxyxy telefax tty', 3); (o['telefax'].backw['zyxyxy'].backw['cdda'].prob == 0.5 && o['telefax'].backw['zyxyxy'].backw['.'].prob == 0.5)",
+        "'multiple sentences'; var o = new nGrams('zyxyxy telefax. angh cdda! zyxyxy telefax angh.', 3); (o['zyxyxy'].forw['telefax'].forw['.'].prob == 0.5 && o['angh'].forw['cdda'].forw['.'].prob == 1.0)",
+        "'n == 5'; var o = new nGrams('zyxyxy telefax. angh cdda! zyxyxy telefax angh.', 5); (o['cdda'].forw['.'].forw['.'].forw['.'].forw['.'].prob == 1.0)"
+    ])
 }
 // Run them automatically:
 tests()
